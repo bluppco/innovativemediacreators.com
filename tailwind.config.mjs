b1786532569cfe353fc667126f97aa8af740389b
Nandini <nandiniverma202021@gmail.com>
2024-08-14 16:42:32 +0530

    feat: created paragraphs for home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -47,7 +47,11 @@ module.exports = {
 			},
 			colors: {
 
-				"red-light":"#fe5b49"
+				"red-light":"#fe5b49",
+				"light-blue":"#00d3ec",
+				"gray":"#5b5b5b",
+				"light-gray":"#7a7a7a",
+				"wheat":"#dcdab8",
 			},
 			fontFamily: {
 
